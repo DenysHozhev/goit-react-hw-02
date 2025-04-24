55e9ffc6884425838d95a7d9c2ef324268ac5f60
Denys <gozhievd@gmail.com>
2025-04-24 20:28:09 +0200

    first

---
+++ src/components/App.jsx
@@ -0,0 +1,66 @@
+import Description from "./description/Description";
+import Options from "./options/Options";
+import "./App.css";
+import Feedback from "./feedback/Feedback";
+import { useState, useEffect } from "react";
+import Notification from "./notification/Notification";
+
+function App() {
+  const [totals, setTotals] = useState(() => {
+    const savedScore = localStorage.getItem("savedScoreKey");
+    if (savedScore !== null) {
+      return JSON.parse(savedScore);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
+
+  useEffect(
+    () => localStorage.setItem("savedScoreKey", JSON.stringify(totals)),
+    [totals]
+  );
+
+  const [openFeedback, setOpenFeedback] = useState(false);
+
+  const totalFeedback = totals.good + totals.neutral + totals.bad;
+
+  const updateFeedback = (feedbackType) => {
+    if (feedbackType === "reset") {
+      setTotals({
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      });
+      setOpenFeedback(false);
+    } else {
+      setTotals({
+        ...totals,
+        [feedbackType]: totals[feedbackType] + 1,
+      });
+      setOpenFeedback(true);
+    }
+  };
+
+  const positiveFeedback = Math.round((totals.good / totalFeedback) * 100);
+
+  return (
+    <>
+      <Description />
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
+      {openFeedback ? (
+        <Feedback
+          totals={totals}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
+}
+
+export default App;
